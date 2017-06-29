5ebd9df0e914b16809f2c06d16cfda9b6c35f9d5
Jane <janeparkjp@gmail.com>
2017-06-29 19:42:06 -0400

    edited sql.js

--- util/sql.js
+++ util/sql.js
@@ -8,9 +8,9 @@ if (process.env.DATABASE_URL) {
 
 else {
 	sql = new Sequelize({
-		database: "foodblog",
-		username: "postgres",
-		password: "postgres123",
+		database: process.env.DB_NAME,
+		username: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
 		host: process.env.DB_HOST || "localhost",
 		port: process.env.DB_PORT || 5432,
 		dialect: "postgres",
